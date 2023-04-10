7673e1176114767c9145796b0a259da3deb195ae
filippobartolucci <filippo.bartolucci2@studio.unibo.it>
2023-04-10 16:51:41 +0200

    Implementazione behaviour car:main. Main rilancia i sotto attori in caso terminino.

--- car.erl
+++ car.erl
@@ -1,36 +1,54 @@
 -module(car).
--export([main/0]).
+-export([main/0, loop/3, friendship/0, state/0, detect/0]).
 
 main() ->
-    spawn_link(?MODULE, friendship, []),
-    spawn_link(?MODULE, detect, []),
-    spawn_link(?MODULE, state, []),
-    loop().
+    {F, _} = spawn_monitor(fun () -> friendship() end),
+    {S, _} = spawn_monitor(fun () -> state() end),
+    {D, _} = spawn_monitor(fun () -> detect() end),
 
-loop() ->
+    loop(F, S, D).
+
+loop(F, S, D) ->
     receive
-        {'EXIT', _Friendship, _Reason} ->
-            io:format("Friendship actor crashed!~n"),
-            spawn_link(?MODULE, friendship, []),
-            loop();
-        {'EXIT', _State, _Reason} ->
-            io:format("State actor crashed!~n"),
-            spawn_link(?MODULE, state, []),
-            loop();
-        {'EXIT', _Detect, _Reason} ->
-            io:format("Detect actor crashed!~n"),
-            spawn_link(?MODULE, detect, []),
-            loop()
-    end.
+        {'DOWN', _Ref, process, Pid, _Reason} ->
+            case Pid of
+                F -> 
+                    io:format("Friendship process died~n"),
+                    {NewPid, _} = spawn_monitor(fun friendship/0),
+                    loop(NewPid, S, D);
+                S -> 
+                    io:format("State process died~n"),
+                    {NewPid, _} = spawn_monitor(fun state/0),
+                    loop(F, NewPid, D);
+                D -> 
+                    io:format("Detect process died~n"),
+                    {NewPid, _} = spawn_monitor(fun detect/0),
+                    loop(F, S, NewPid);
+                _ ->
+                    io:format("Unknown process~n with pid: ~p~n", [Pid]),
+                    loop(F, S, D)
+            end;
+        _ ->
+            io:format("Unknown message~n"),
+            loop(F, S, D)
+    end.   
 
 friendship() ->
-    pass.
+    % sleep for 5 seconds
+    timer:sleep(1000),  
+    1/0,
+    io:format("Friendship bye bye~n").
 
 state() ->
-    pass.
+    % sleep for 10 seconds
+    timer:sleep(20000),
+    io:format("State bye bye~n").
 
 detect() ->
-    pass.
+    % sleep for 15 seconds
+    timer:sleep(50000),
+    io:format("Detect bye bye~n").
+
 
 
 
